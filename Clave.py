c616042bfff7707a9d69502f612492847529ae3b
Alicia Sáenz de Zaítigui Colina <aliciasaenz1@hotmail.es>
2024-10-28 15:41:29 +0100

    link

---
+++ Clave.py
@@ -0,0 +1,5 @@
+
+class Claves():
+    def __init__(self, usuario, key,):
+
+#https://code-with-me.global.jetbrains.com/c4hIf_ReOKorXi81a65ziA#p=PY&fp=45AF01845B6A906BDE1DF0DBA6A27766C78319EBBD52A735BFBAF1A6820CEF17&newUi=true
